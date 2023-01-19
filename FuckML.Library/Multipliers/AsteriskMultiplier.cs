885de3dc1a4c17338f40d1aef8982747d69a153d
Alexey <87594443+alekami649@users.noreply.github.com>
2023-01-19 19:52:25 +0700

    asterisk

--- FuckML.Library/Multipliers/AsteriskMultiplier.cs
+++ FuckML.Library/Multipliers/AsteriskMultiplier.cs
@@ -13,45 +13,6 @@
 
             result.Add(sourceArray);
 
-            for (int i = 0; i < sourceArray.Length; i++) 
-            {
-                var temp = sourceArray;
-                temp[i] = '*';
-                result.Add(temp);
-            }
-
-
-            for (int i = 0; i < source.Length; i++)
-            {
-                var temp = sourceArray;
-
-                temp[i] = '*';
-
-                result.Add(temp);
-            }
-
-            if (source.Length > 2)
-            {
-                for (int i = 0; i > source.Length - 1; i++)
-                { 
-                    var temp = sourceArray;
-
-                    temp[i] = '*';
-                    temp[i + 1] = '*';
-
-                    result.Add(temp);
-                }
-                for (int i = source.Length; i == 0; i--)
-                {
-                    var temp = sourceArray;
-
-                    temp[i + 1] = '*';
-                    temp[i] = '*';
-
-                    result.Add(temp);
-                }
-            }
-
             return result.Select(x => new string(x)).ToArray();
         }
     }
--- FuckML.Library/Searchers/RootSearcher.cs
+++ FuckML.Library/Searchers/RootSearcher.cs
@@ -2,13 +2,14 @@
 {
     public class RootSearcher : ISearcher
     {
-        public bool Recurse { get; set; } = true;
-
         public bool ContainsObscene(string msg)
         {
             if (string.IsNullOrEmpty(msg))
                 return false;
 #pragma warning disable CA1307 // Используйте StringComparison, чтобы ясно указать намерение.
+
+            if (msg.Contains("**"))
+                return true;
 
             #region Russain-Only Obsense
             if (msg.Contains("хуе") || msg.Contains("хуй") || msg.Contains("нах"))
@@ -23,18 +24,20 @@
                 return true;
             #endregion
 
-            //#region Russain-Only Asterisk Obsense
-            //if (msg.Contains("хуе") || msg.Contains("хуй"))
-            //    return true;
-            //else if (msg.Contains("член"))
-            //    return true;
-            //else if (msg.Contains("бля"))
-            //    return true;
-            //else if (msg.Contains("пиз"))
-            //    return true;
-            //else if (msg.Contains("еб") || msg.Contains("ёб"))
-            //    return true;
-            //#endregion
+            #region Russain-Only Asterisk Obsense
+            if (msg.Contains("*уе") || msg.Contains("х*е") || msg.Contains("ху*"))
+                return true;
+            else if (msg.Contains("*уй") || msg.Contains("х*й") || msg.Contains("ху*"))
+                return true;
+            else if (msg.Contains("*лен") || msg.Contains("ч*ен")  || msg.Contains("чл*н") || msg.Contains("чле*"))
+                return true;
+            else if (msg.Contains("*ля") || msg.Contains("б*я") || msg.Contains("бл*"))
+                return true;
+            else if (msg.Contains("п**") || msg.Contains("п*з") || msg.Contains("пи*"))
+                return true;
+            else if (msg.Contains("е*") || msg.Contains("ё*") || msg.Contains("*б"))
+                return true;
+            #endregion
 
             #region Russian-English One-Design Obsense
             if (msg.Contains("xye") || msg.Contains("xyй") || msg.Contains("нaх"))
