79f1089a2ef62c95f750ce9d5ca19f0342063130
Alexey <87594443+alekami649@users.noreply.github.com>
2023-01-19 19:15:11 +0700

    "нах" support

--- FuckML.Library/Searchers/RootSearcher.cs
+++ FuckML.Library/Searchers/RootSearcher.cs
@@ -23,18 +23,18 @@
                 return true;
             #endregion
 
-            #region Russain-Only Asterisk Obsense
-            if (msg.Contains("хуе") || msg.Contains("хуй"))
-                return true;
-            else if (msg.Contains("член"))
-                return true;
-            else if (msg.Contains("бля"))
-                return true;
-            else if (msg.Contains("пиз"))
-                return true;
-            else if (msg.Contains("еб") || msg.Contains("ёб"))
-                return true;
-            #endregion
+            //#region Russain-Only Asterisk Obsense
+            //if (msg.Contains("хуе") || msg.Contains("хуй"))
+            //    return true;
+            //else if (msg.Contains("член"))
+            //    return true;
+            //else if (msg.Contains("бля"))
+            //    return true;
+            //else if (msg.Contains("пиз"))
+            //    return true;
+            //else if (msg.Contains("еб") || msg.Contains("ёб"))
+            //    return true;
+            //#endregion
 
             #region Russian-English One-Design Obsense
             if (msg.Contains("xye") || msg.Contains("xyй") || msg.Contains("нaх"))
@@ -46,7 +46,7 @@
             #endregion
 
             #region Russian-English Translit Obsense
-            if (msg.Contains("xyi") )
+            if (msg.Contains("xyi"))
                 return true;
             else if (msg.Contains("chlen"))
                 return true;
